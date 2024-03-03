4e6e3fc06175e04c1223ea380e9b6bc1544bc4f6
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2024-03-03 03:44:30 -0500

    Fix cpu usage

--- src/ServerListener.cpp
+++ src/ServerListener.cpp
@@ -54,6 +54,7 @@ void ServerListener::connect(){
                 geode::log::info("{}", message);
 
                 });
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
     }
 
--- src/easywsclient.cpp
+++ src/easywsclient.cpp
@@ -15,6 +15,8 @@
     #include <string.h>
     #include <sys/types.h>
     #include <io.h>
+    #include <chrono>
+    #include <thread>
     #ifndef _SSIZE_T_DEFINED
         typedef int ssize_t;
         #define _SSIZE_T_DEFINED
@@ -225,6 +227,7 @@ class _RealWebSocket : public easywsclient::WebSocket
             else {
                 rxbuf.resize(N + ret);
             }
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
         while (txbuf.size()) {
             int ret = ::send(sockfd, (char*)&txbuf[0], txbuf.size(), 0);
@@ -241,6 +244,7 @@ class _RealWebSocket : public easywsclient::WebSocket
             else {
                 txbuf.erase(txbuf.begin(), txbuf.begin() + ret);
             }
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
         if (!txbuf.size() && readyState == CLOSING) {
             closesocket(sockfd);
@@ -362,6 +366,7 @@ class _RealWebSocket : public easywsclient::WebSocket
             else { fprintf(stderr, "ERROR: Got unexpected WebSocket message.\n"); close(); }
 
             rxbuf.erase(rxbuf.begin(), rxbuf.begin() + ws.header_size+(size_t)ws.N);
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
     }
 
