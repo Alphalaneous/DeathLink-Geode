a920ce1785b24ec345154b8d50c898500d96b37e
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2024-03-09 21:45:47 -0500

    Okay maybe too much

--- src/ServerListener.cpp
+++ src/ServerListener.cpp
@@ -86,7 +86,7 @@ void ServerListener::connect(){
                 //geode::log::info("{}", message);
 
                 });
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     }
 
